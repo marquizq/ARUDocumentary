f47df316af5111e61d12ed2d28607c941fe0792a
Tonny <anjige206@gmail.com>
2018-12-17 12:44:19 +0300

    New word Documentary Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,7 @@
                 <div class="title m-b-md">
                     ARU Documentary
                     SERBI School
+                    Documentary
                 </div>
 
                 <div class="links">
