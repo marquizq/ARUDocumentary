477da9fdc3e103d798c62c2688903a80e92a8887
damian1094 <patrickdamian2012@gmail.com>
2019-01-02 16:09:34 +0300

    footer added to both service and contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,6 +9,24 @@
 </head>
 
 <body>
+	<div class="footer">
+	<p>@2019 Jesca Kapingu</p>
 
+	</div>
+
+	<style>
+.footer {
+
+position: fixed;
+left: 0;
+bottom: 0;
+height: 8%;
+width: 100%;
+background-color: black;
+color: white;
+text-align: center;
+}
+
+</style>
 </body>
 </html>
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -8,6 +8,25 @@
 <body>
 
 <h1> Documentary </h1>
+<div class="footer">
+<p>@2019 Jesca Kapingu</p>
+
+</div>
+
+<style>
+.footer {
+
+position: fixed;
+left: 0;
+bottom: 0;
+height: 8%;
+width: 100%;
+background-color: black;
+color: white;
+text-align: center;
+}
+
+</style>
 
 </body>
 </html>
