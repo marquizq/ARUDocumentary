2fe6068fd697b54303d3c136d62312251bba91df
damian1094 <patrickdamian2012@gmail.com>
2019-01-03 19:43:38 +0300

    update on contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,21 +1,3 @@
+
 @extends('layouts.app1')
-@section('content')
-
-<div class="container">
-	<div class="our-contact">
-		<h4>Phone number</h2>
-		<ul class="pnumber">
-			<li>+255(0)752 307887</li>
-			<li>+255(0)742 859904</li>
-			<li>+255(0)654 275921</li>
-			<li>+255(0)715 599346</li>
-		</ul> <br>
-		<h4>e-mail</h2>
-		<ul class="pnumber">
-			<li>csm-aru@gmail.com</li>
-		</ul>
-
-
-	</div>
-</div>
-@endsection
+@include('inc.form')
